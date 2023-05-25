ab790723c650cff7d95f3b90312cdc72d3676036
Andrew Mamchyn <mamchinn@gmail.com>
2023-05-25 18:10:13 -0400

    added two components to display list of songs, added scss file to style those components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Player from "./components/Player";
 import "./styles/App.scss"
 import data from "./data";
 import {useState} from "react";
+import Library from "./components/Library";
 
 function App() {
 
-  const [song, setSong] = useState(data())
-  const [currentSong, setCurrentSong] = useState(song[0])
+  const [songs, setSongs] = useState(data())
+  const [currentSong, setCurrentSong] = useState(songs[0])
   const [isPlaying, setIsPlaying] = useState(false)
 
   return (
@@ -20,6 +21,7 @@ function App() {
         isPlaying={isPlaying}
         setIsPlaying={setIsPlaying}
       />
+      <Library songs={songs}/>
     </div>
   );
 }
---
+++ src/components/LibrarySong.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function LibrarySong({song}) {
+    return (
+        <div className='library-song'>
+            <img
+                alt={song.name}
+                src={song.cover}
+            />
+            <div className='song-description'>
+                <h3>{song.name}</h3>
+                <h4>{song.artist}</h4>
+            </div>
+        </div>
+    );
+}
+
+export default LibrarySong;
